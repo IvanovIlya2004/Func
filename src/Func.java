ed8ce2b58bf97b015b73ff303b8de8b9c701352b
Ilya.Ivanov <ilya.ivanov.ki-25@mail.ru>
2022-12-03 19:39:44 +0300

    hello

--- src/Func.java
+++ src/Func.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Func {
+import java.util.Scanner;
+public class Func {
+    public void func() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите функцию x:");
+        String func = sc.nextLine();
+        sc.close();
+        if (func.equalsIgnoreCase("x")) {
+            System.out.println("Производная равна 1");
+        }
+        else {
+            System.out.println("Вы ввели не x");
+        }
+    }
 }
